48d3f7f7d0200fcc58edadc331f0f3ce6d199e6a
Thomas Dwi <mastomcsc23@gmail.com>
2022-06-04 11:15:14 +0700

    selesai no 3

--- Tugas 5/dua.py
+++ Tugas 5/dua.py
@@ -12,8 +12,8 @@ def get_soup(url):
 #     print(i.get('href'))
 
 def getGoPackage(msg, num):
-    url = "https://www.google.com/search?q=" + msg
-    soup = get_soup("https://go.dev/doc/")
+    url = "https://pkg.go.dev/search?q=" + msg
+    soup = get_soup(url)
     #Get value tag a with beautifoulsoup in article tag
     tag = soup.find_all('article')
     
@@ -34,8 +34,36 @@ def getGoPackage(msg, num):
     
     return True
 
+def getGoBlog(msg):
+    url = "https://pkg.go.dev/search?q=" + msg
+    soup = get_soup(url)
+
+    #Get value with beautifoulsoup in class SearchSnippet
+    SearchSnippet = soup.find_all('div', class_='SearchSnippet')
+
+    #Get href with beautifulsoup in class SearchSnippet
+    judul = SearchSnippet[0].find_all('a')
+    url_hasil_search = "https://pkg.go.dev/" + judul[0].get('href')
+    soup = get_soup(url_hasil_search)
+
+    #Get value with beautifoulsoup in class Documentation-indexList
+    Documentation_indexList = soup.find_all('ul', class_='Documentation-indexList')
+
+    #Get value li with beautifoulsoup
+    Documentation_indexList_li = Documentation_indexList[0].find_all('a')
+
+    # Get all text with beautifulsoup
+    for i in Documentation_indexList_li:
+        text = i.get_text().split(' ')
+        if (text[0] == 'type'):
+            print(i.get_text())
+        elif (text[0] == 'func'):
+            print('\t' + i.get_text())
+    return True
+
 def main():   
-    getGoPackage("llrb", 10)
+    # getGoPackage("llrb", 10)
+    getGoBlog("llrb+petar")
 
 if __name__ == '__main__':
     main()
